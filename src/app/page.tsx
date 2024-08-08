0a38329af1f35ac21ba0680d2f709fc760d8dca3
aguywhosaguy <48103075+aguywhosaguy@users.noreply.github.com>
2024-08-07 22:09:22 -0500

    fix: changed wording on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
         <div className="space-y-3">
           <p>Hello! I&#39;m aguywhosaguy. This is the website where i&#39;ll keep all of my projects. It&#39;s quite empty at the moment, as I haven&#39;t gotten a lot done.</p>
           <p>While I haven&#39;t really found my place yet, I&#39;m interested in web development. However, frontend isn&#39;t really my strong suit, if you couldn&#39;t tell by my masterful web design.</p>
-          <p>My main goal throughout my various coding projects is to make things easier (like the rest of programming), partically in the video gaming space. While not every project I&#39;ve made pertains to gaming, making mods and programs for games is what got me into this whole programming thing in the first place.</p>
+          <p>My main goal throughout my various coding projects is to make things easier (which is the point of programming), partically in the video gaming space. While not every project I&#39;ve made pertains to gaming, making mods and programs for games is what got me into programming in the first place.</p>
           <p></p>
         </div>
         <div className="space-y-4">
@@ -16,7 +16,7 @@ export default function HomePage() {
               <li><a href="https://youtube.com/aguywhosaguy" className="font-bold underline">YouTube</a></li>
             </ul>
             <p>I also go by the same username on Discord, @aguywhosaguy.</p>
-            <p>Check out the projects tab to see what I&#39;m up to. At the time of writing this, though, I&#39;m not proud of anything there.</p>
+            <p>Check out the projects tab to see what I&#39;m up to. At the time of writing this, however, the projects there aren't representative of my current programming skills.</p>
         </div>
       </div>
     </main>
