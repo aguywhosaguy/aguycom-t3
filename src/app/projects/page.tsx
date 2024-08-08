7baa691a5a52fbd9a6beb5c25fb96b43b8485faf
aguywhosaguy <caseypeg8@gmail.com>
2024-08-07 20:53:43 -0500

    release: finish projects system, finished main page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,29 @@
-import ProjectCard from "@/lib/components/ProjectCard";
+import {ProjectCard, Tag, CardDetails} from "@/lib/components/ProjectCard";
 
-export enum Tag {
-	Website = "website",
-	Package = "package"
-}
+
+
+const Projects: CardDetails[] = [
+	{
+		"name": "Thirty Dollar Extension",
+		"tag": Tag.Extension,
+		"description": "An extension that adds custom sounds to https://thirtydollar.website/",
+		"url": "https://github.com/aguywhosaguy/thirtydollarcustomsounds"
+	}
+]
 
 export default function ProjectsPage() {
 	return (
 	  <main className="px-20 py-30 my-20 mx-auto max-w-5xl">
 		<div className="space-y-5">
-		  <h1 className="text-5xl font-bold">Projects</h1>
-		  <div className="flex flex-wrap justify-center align-middle border-4 border-black max-w-5xl gap-10 p-4">
-		  </div>
+			<h1 className="text-5xl font-bold">Projects</h1>
+			<div className="flex flex-wrap justify-center align-middle border-4 border-black max-w-5xl gap-10 p-4">
+			{Projects.map((project: CardDetails) => (
+				<ProjectCard details={project} key={project.name}/>
+			))}
+			</div>
 		</div>
 		<div className="w-4"></div>
 	  </main>
-	);
-  }
+  );
+}
   
--- src/lib/components/ProjectCard.tsx
+++ src/lib/components/ProjectCard.tsx
@@ -1,18 +1,25 @@
-import { Tag } from "@/app/projects/page";
-
-interface CardDetails {
+export interface CardDetails {
     name: string;
-    tags: Tag[];
+    tag: Tag;
     description: string;
     url: string;
 }
 
-export default function ProjectCard({details}: {"details": CardDetails}
+export enum Tag {
+	Website = "website",
+	Package = "package",
+	Extension = "extension"
+}
 
-) {
+export function ProjectCard({details}: {"details": CardDetails}) {
     return (
-    <div className="border-2 w-64 h-48">
+    <div className="relative border-2 w-64 h-48">
+        <div className="absolute top-0 left-0 p-2 font-bold justify-start">
+            {details.name}
+        </div>
+        <div>
 
+        </div>
     </div>
     )
 }
